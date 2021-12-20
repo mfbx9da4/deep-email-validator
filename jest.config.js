67fafbba8f0a453e984893baef2dd58f3c67ebac
David Alberto Adler <dalberto.adler@gmail.com>
2021-12-20 20:26:10 +0000

    update deps

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-    globals: {
-        "ts-jest": {
-            tsConfig: "tsconfig.json"
-        }
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
     },
-    moduleFileExtensions: [
-        "ts",
-        "js"
-    ],
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest"
-    },
-    testMatch: [
-        "**/test/**/*.test.(ts|js)"
-    ],
-    testEnvironment: "node"
-};
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/test/**/*.test.(ts|js)'],
+  testEnvironment: 'node',
+  testTimeout: 10000,
+}
